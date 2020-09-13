bde301cb4114a8018a58761b81aa8cd6e2202f0f
Caspeezie <sadikutaahmad2020@gmail.com>
2020-09-13 12:47:36 +0100

    Second commit

--- Grace/src/IfStatement/forCount.java
+++ Grace/src/IfStatement/forCount.java
@@ -3,8 +3,9 @@ package IfStatement;
 public class forCount {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int number = 1;
+		
+			
 	}
 
 }
---
+++ Grace/src/IfStatement/product.java
@@ -0,0 +1,13 @@
+package IfStatement;
+
+public class product {
+
+	public static void main(String[] args) {
+	      int product = 1;
+
+	      for ( int x = 1; x <= 15; x += 2 )
+	         product *= x;
+	      
+	      System.out.printf( "Product is %d\n", product );      
+   } 
+} 
